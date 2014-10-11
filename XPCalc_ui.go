12859e893802a1b874e83353c412cf8c12ac29e6
SayCV <sssxyc@163.com>
2014-10-11 15:47:02 +0800

    ...

--- XPCalc_ui.go
+++ XPCalc_ui.go
@@ -74,7 +74,16 @@ func (w *Dialog) init(owner walk.Form) (err error) {
 	if w.Dialog, err = walk.NewDialog(owner); err != nil {
 		return err
 	}
-
+	
+	// Left Top Width Height
+	gap_x := 2
+	gap_y := 2
+	pos_x := gap_x
+	pos_y := gap_y
+	add_w := 0
+	add_h := 0
+	line_pos := 0
+	
 	succeeded := false
 	defer func() {
 		if !succeeded {
@@ -95,39 +104,205 @@ func (w *Dialog) init(owner walk.Form) (err error) {
 		return err
 	}
 	
+	pos_x = gap_x; pos_y = gap_y; add_w = 480; add_h = 32
 	// textEdit
 	if w.ui.textEdit, err = walk.NewTextEdit(w); err != nil {
 		return err
 	}
 	w.ui.textEdit.SetName("textEdit")
-	if err := w.ui.textEdit.SetBounds(walk.Rectangle{0, 0, 568, 32}); err != nil {
+	if err := w.ui.textEdit.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
 		return err
 	}
-
+	
+	pos_x = gap_x; pos_y += gap_y + add_h; add_w = 270; add_h = 32
+	line_pos = pos_y
 	// gBoxNumberNotation
 	if w.ui.gBoxNumberNotation, err = walk.NewGroupBox(w); err != nil {
 		return err
 	}
 	w.ui.gBoxNumberNotation.SetName("gBoxNumberNotation")
-	if err := w.ui.gBoxNumberNotation.SetBounds(walk.Rectangle{0, 64, 0, 32}); err != nil {
+	if err := w.ui.gBoxNumberNotation.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
 		return err
 	}
-	if err := w.ui.gBoxNumberNotation.SetTitle(`gBoxNumberNotation`); err != nil {
-		return err
-	}
+	//if err := w.ui.gBoxNumberNotation.SetTitle(nil); err != nil {
+	//	return err
+	//}
 	
+	pos_x = gap_x; pos_y = gap_y * 6; add_w = (add_w - (gap_y * 6))/4; add_h = 18
 	// rbNumberNotation_16
-	if w.ui.rbNumberNotation_16, err = walk.NewToolButton(w.ui.gBoxNumberNotation); err != nil {
+	if w.ui.rbNumberNotation_16, err = walk.NewRadioButton(w.ui.gBoxNumberNotation); err != nil {
 		return err
 	}
 	w.ui.rbNumberNotation_16.SetName("rbNumberNotation_16")
-	if err := w.ui.rbNumberNotation_16.SetBounds(walk.Rectangle{10, 12, 59, 18}); err != nil {
+	if err := w.ui.rbNumberNotation_16.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
 		return err
 	}
-	if err := w.ui.rbNumberNotation_16.SetText(`Connected`); err != nil {
+	if err := w.ui.rbNumberNotation_16.SetText(`16bin`); err != nil {
 		return err
 	}
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// rbNumberNotation_10
+	if w.ui.rbNumberNotation_10, err = walk.NewRadioButton(w.ui.gBoxNumberNotation); err != nil {
+		return err
+	}
+	w.ui.rbNumberNotation_10.SetName("rbNumberNotation_10")
+	if err := w.ui.rbNumberNotation_10.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.rbNumberNotation_10.SetText(`10bin`); err != nil {
+		return err
+	}
+	w.ui.rbNumberNotation_10.SetChecked(true)
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// rbNumberNotation_08
+	if w.ui.rbNumberNotation_08, err = walk.NewRadioButton(w.ui.gBoxNumberNotation); err != nil {
+		return err
+	}
+	w.ui.rbNumberNotation_08.SetName("rbNumberNotation_08")
+	if err := w.ui.rbNumberNotation_08.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.rbNumberNotation_08.SetText(`08bin`); err != nil {
+		return err
+	}
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// rbNumberNotation_02
+	if w.ui.rbNumberNotation_02, err = walk.NewRadioButton(w.ui.gBoxNumberNotation); err != nil {
+		return err
+	}
+	w.ui.rbNumberNotation_02.SetName("rbNumberNotation_02")
+	if err := w.ui.rbNumberNotation_02.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.rbNumberNotation_02.SetText(`02bin`); err != nil {
+		return err
+	}
+	
+	pos_x += add_w + gap_x * 10; pos_y = line_pos; add_w = 270; add_h = 32
+	// gBoxNumberUnitBytes
+	if w.ui.gBoxNumberUnitBytes, err = walk.NewGroupBox(w); err != nil {
+		return err
+	}
+	w.ui.gBoxNumberNotation.SetName("gBoxNumberUnitBytes")
+	if err := w.ui.gBoxNumberUnitBytes.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	//if err := w.ui.gBoxNumberUnitBytes.SetTitle(nil); err != nil {
+	//	return err
+	//}
+	
+	pos_x = gap_x; pos_y = gap_y * 6; add_w = (add_w - (gap_y * 6))/4; add_h = 18
+	// rbNumberUnitBytes_08
+	if w.ui.rbNumberUnitBytes_08, err = walk.NewRadioButton(w.ui.gBoxNumberUnitBytes); err != nil {
+		return err
+	}
+	w.ui.rbNumberUnitBytes_08.SetName("rbNumberUnitBytes_08")
+	if err := w.ui.rbNumberUnitBytes_08.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.rbNumberUnitBytes_08.SetText(`ang`); err != nil {
+		return err
+	}
+	w.ui.rbNumberUnitBytes_08.SetChecked(true)
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// rbNumberUnitBytes_04
+	if w.ui.rbNumberUnitBytes_04, err = walk.NewRadioButton(w.ui.gBoxNumberUnitBytes); err != nil {
+		return err
+	}
+	w.ui.rbNumberUnitBytes_04.SetName("rbNumberUnitBytes_04")
+	if err := w.ui.rbNumberUnitBytes_04.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.rbNumberUnitBytes_04.SetText(`rad`); err != nil {
+		return err
+	}
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// rbNumberUnitBytes_02
+	if w.ui.rbNumberUnitBytes_02, err = walk.NewRadioButton(w.ui.gBoxNumberUnitBytes); err != nil {
+		return err
+	}
+	w.ui.rbNumberUnitBytes_02.SetName("rbNumberUnitBytes_02")
+	if err := w.ui.rbNumberUnitBytes_02.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.rbNumberUnitBytes_02.SetText(`gra`); err != nil {
+		return err
+	}
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// rbNumberUnitBytes_01
+	if w.ui.rbNumberUnitBytes_01, err = walk.NewRadioButton(w.ui.gBoxNumberUnitBytes); err != nil {
+		return err
+	}
+	w.ui.rbNumberUnitBytes_01.SetName("rbNumberUnitBytes_01")
+	if err := w.ui.rbNumberUnitBytes_01.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.rbNumberUnitBytes_01.SetText(`Byte`); err != nil {
+		return err
+	}
+	w.ui.rbNumberUnitBytes_01.SetVisible(false)
+	
+	line_pos += 32
+	add_w = 0
 
+	pos_x += add_w + gap_x * 10; pos_y = line_pos; add_w = 270; add_h = 32
+	// gBoxFunctionOps
+	if w.ui.gBoxFunctionOps, err = walk.NewGroupBox(w); err != nil {
+		return err
+	}
+	w.ui.gBoxFunctionOps.SetName("gBoxFunctionOps")
+	if err := w.ui.gBoxFunctionOps.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	//if err := w.ui.gBoxFunctionOps.SetTitle(nil); err != nil {
+	//	return err
+	//}
+	
+	pos_x += gap_x; pos_y = gap_y * 6; add_w = (add_w - (gap_y * 6))/4; add_h = 18
+	// tbBackspace
+	if w.ui.tbBackspace, err = walk.NewToolButton(w.ui.gBoxFunctionOps); err != nil {
+		return err
+	}
+	w.ui.tbBackspace.SetName("tbBackspace")
+	if err := w.ui.tbBackspace.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.tbBackspace.SetText(`Backspace`); err != nil {
+		return err
+	}
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// tbClearError
+	if w.ui.tbClearError, err = walk.NewToolButton(w.ui.gBoxFunctionOps); err != nil {
+		return err
+	}
+	w.ui.tbClearError.SetName("tbClearError")
+	if err := w.ui.tbClearError.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.tbClearError.SetText(`CE`); err != nil {
+		return err
+	}
+	
+	pos_x += gap_x + add_w; pos_y += 0; add_w += 0; add_h += 0
+	// tbClear
+	if w.ui.tbClear, err = walk.NewToolButton(w.ui.gBoxFunctionOps); err != nil {
+		return err
+	}
+	w.ui.tbClear.SetName("tbClear")
+	if err := w.ui.tbClear.SetBounds(walk.Rectangle{pos_x, pos_y, add_w, add_h}); err != nil {
+		return err
+	}
+	if err := w.ui.tbClear.SetText(`C`); err != nil {
+		return err
+	}
+	
 	succeeded = true
 
 	return nil
